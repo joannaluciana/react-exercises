6b327db3def84b84a9877521db3e4b1414d84ccc
joannaluciana <joanna@wloskowicz.pl>
2020-09-23 18:09:03 +0200

    printing student list

---
+++ src/components/models/Students.js
@@ -0,0 +1,43 @@
+
+const codeBrainersStudents = [
+  {
+    name: 'Michał K.',
+    coffees: 1
+  },
+  {
+    name: 'Michał M.',
+    coffees: 1
+  },
+  {
+    name: 'Joanna',
+    coffees: 2
+  },
+  {
+    name: 'Karolina',
+    coffees: 3
+  },
+  {
+    name: 'Grzegorz',
+    coffees: 0
+  },
+  {
+    name: 'Damian',
+    coffees: 1
+  },
+  {
+    // Jedna kawka od Michałów i Damiana!
+    name: 'Sabina',
+    coffees: 2
+  },
+  {
+    name: 'Kamila',
+    coffees: 0
+  },
+  {
+    name: 'Maksym',
+    coffees: 2
+  }
+
+];
+
+export default  codeBrainersStudents;
