4eeb4fb0fe1ea65b214d6c41736b195a23715a4f
joannaluciana <joanna@wloskowicz.pl>
2020-09-24 18:40:35 +0200

    adding css style to App.js component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import StudentList from "./components/student-list/StudentList";
+import './App.css';
 
 function App() {
     return (
-        <div>
+        <div className="app">
             <h1>Hello world!</h1>
             <StudentList/>
         </div>
