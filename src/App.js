ce58006c0e2d23c9fbc0d1b64ffd1ad317c0cc99
joannaluciana <joanna@wloskowicz.pl>
2020-09-25 15:14:05 +0200

    adding new css style and react style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import StudentList from "./components/student-list/StudentList";
 import './App.css';
 
+// const divStyle = {
+//     color: 'white',
+// };
 function App() {
     return (
-        <div className="app">
+        <div className="app" >
             <h1>Hello world!</h1>
+            <h1 className='small'>Hello world!</h1>
             <StudentList/>
         </div>
     );
--- src/components/student-list/Student.jsx
+++ src/components/student-list/Student.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import './Student.css';
 
 const Student = (props) => {
     const studentName = props.studentName;
     const studentCoffees = props.studentCoffees;
+    const divStyle = {
+    paddingLeft: studentCoffees + 'em',
+};
 
 
     return (
         <tr>
-            <td> { studentName} </td>
+            <td>
+                <div className='student' style = {divStyle}>
+                { studentName}
+            </div>
+            </td>
             <td> { studentCoffees} </td>
         </tr>
     );
